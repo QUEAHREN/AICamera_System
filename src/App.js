abfc45a83e0c963a93bcb4353d0aa9ecbf3fc9b3
QUEAHREN <xsyooo@icloud.com>
2021-10-27 14:01:43 +0800

    Add some pages

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './assets/images/logo.svg';
+import './assets/css/App.css';
+
+
+function App() {
+  return (
+    <div></div>
+    
+  );
+}
+
+export default App;
---
+++ src/pages/UsrLogin.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+
+class UsrLogin extends React.Component{
+
+
+    constructor(props){
+
+        super(props);
+
+    }
+
+    render(){
+        return(
+            <div>
+                
+            </div>
+        )
+    }
+
+
+    
+    
+}
+
+export default UsrLogin;
