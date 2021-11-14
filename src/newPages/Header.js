a7804e3efaab9a1193a5fd70a31f8d771264b8db
QUEAHREN <xsyooo@icloud.com>
2021-11-14 19:21:01 +0800

    Add some notes,wait to test preview

--- src/newPages/Header.js
+++ src/newPages/Header.js
@@ -116,6 +116,7 @@ function Header(props) {
                 </Toolbar>
             </AppBar>
             <AppBar component="div" position="static" elevation={0} sx={{ zIndex: 0 }}>
+                {/* 为了使每次切换navigator目录时能把header切换回首个选项，所以需要读取父组件的值 */}
                 <Tabs value={hvalue===0?hvalue:value} textColor="inherit" onChange={(event,newvalue) => handleButtonClick(event, newvalue)} >
                     {chmenu.map(({ id, active }, index) => (                      
                         <Tab label={id} selected={active} value={index} />
--- src/router/RouterConfig.js
+++ src/router/RouterConfig.js
@@ -1,6 +1,6 @@
 
 import Config from '../pages/Config';
-import UsrLogin from '../pages/UsrLogin';
+import UsrLogin from '../newPages/UsrLogin';
 import Preview from '../pages/Preview';
 import Other from '../pages/Other';
 import Test from '../pages/Test';
@@ -8,6 +8,7 @@ import Test from '../pages/Test';
 import React from 'react';
 import {Router,Route,Switch,Redirect} from 'react-router-dom';
 import { createHashHistory } from "history";
+import Paperbase from '../newPages/Paperbase';
 const history = createHashHistory();
 
 class RouterConfig extends React.Component{
@@ -19,6 +20,7 @@ class RouterConfig extends React.Component{
                         <Redirect to='/Preview'/>
                     )}/>
                     <Route path='/Login' component={UsrLogin}/>
+                    <Route path='/Index' component={Paperbase}/>
                     <Route path='/Config' component={Config}/>
                     <Route path='/Preview' component={Preview}/>
                     <Route path='/Other' component={Other}/>                   
